ddd343a52a44d881ae2e1e00f688e6ae420dd225
0xBipBip <oxbipbip@gmail.com>
2023-09-16 10:07:05 -0300

    added class

--- FirstApp/app/src/main/java/com/example/firstapp/MainActivity.kt
+++ FirstApp/app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -4,5 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 fun main() {
-    println("Helllo World!")
+    val example = Example()
+    println(example.print())
+}
+
+class Example () {
+    fun print() {
+        println("HOLAAAA")
+    }
 }
